237ea967450e8e436c2dcf0c839ea21279c1d435
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-16 09:55:18 +0100

    feat: styling for the landing page
    fix #1

---
+++ pages/_document.jsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class myDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Exo:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default myDocument;
