9a127260b4f9f85973578cbebc4a3da1703107cf
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-27 10:15:13 +0100

    fix: timeout error

--- pages/api/phones.js
+++ pages/api/phones.js
@@ -11,8 +11,10 @@ async function getPhones(phone) {
 
 	const browser = await chromium.puppeteer.launch({
 		args: chromium.args,
+		defaultViewport: chromium.defaultViewport,
 		executablePath: await chromium.executablePath,
 		headless: true,
+		ignoreHTTPSErrors: true,
 	});
 	const page = await browser.newPage();
 	await page.goto('https://www.gsmarena.com/');
